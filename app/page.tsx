e4d41eec583ae7a860ddb8884e08acf346260305
ymj5726-lgtm <ymj5726@gmail.com>
2026-05-06 15:36:48 +0900

    Update page_260506 1.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react'
 import PocketBase from 'pocketbase'
 
-const pb = new PocketBase('http://127.0.0.1:8090')
+const pb = new PocketBase('http://192.168.70.27:8090')
 
 type RequestItem = {
   id: string
